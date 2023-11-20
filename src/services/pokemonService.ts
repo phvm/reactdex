86f41e5702cd0f67f27cd92947e76012f06e3ea1
Pedro Moura <pvhm@cin.ufpe.br>
2023-11-20 18:19:30 -0300

    feat: return data from service

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -2,7 +2,7 @@ import { api } from "./api.ts";
 
 export class PokemonService {
   static async get(id: number) {
-    const response = await api.get(`${id}`);
-    return response;
+    const { data } = await api.get(`${id}`);
+    return data;
   }
 }
