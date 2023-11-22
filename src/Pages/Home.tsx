b7b18d1c15d63965e08e57f1f638ea6e4c93c050
phvm <pedrohvdm@gmail.com>
2023-11-22 09:42:49 -0300

    fix: request to pokeapi

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -10,7 +10,7 @@ export const Home = () => {
   useEffect(() => {
     async function getPokemons() {
       for (let i = 1; i <= count; i++) {
-        await PokemonService.get(i).then((data) => {
+        PokemonService.get(i).then((data) => {
           const pokemon: Pokemon = {
             name: data.name,
             icon: data.sprites.front_default,
